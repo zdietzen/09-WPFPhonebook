b19bc1da16c94660da3b377adbf1b524e38aef28
zdietzen <diezel37@gmail.com>
2015-10-01 14:07:22 -0700

    First commit
    Have the main window roughly built out. Added the first popup window
    which will be built out to add new contacts.

---
+++ 09-WPFPhonebook/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _09_WPFPhonebook
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+                private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        private void MenuItem_Open_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files|*.txt";
+
+            bool? openFile = dialog.ShowDialog();
+
+            if (openFile == true)
+            {
+                string filename = dialog.FileName;
+
+                string fileContents = File.ReadAllText(filename);
+
+                dataGrid.Text = fileContents;
+                // grabs a .txt file and opens it
+            }
+        }
+            private void MenuItem_Save_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+            bool? saveFile = SaveFileDialog1.ShowDialog();
+
+            if (saveFile == true)
+            {
+                using (Stream s = File.Open(SaveFileDialog1.FileName, FileMode.CreateNew))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(dataGrid.Text);
+                }
+            } 
+        }
+
+        private void MenuItem_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void addNewContactButton_Copy_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                AddContactWindow secondWindow = new AddContactWindow();
+
+                secondWindow.Owner = this;
+
+                secondWindow.Show();
+
+                secondWindow.Title = "Add a Contact";
+            }
+
+        }
+    }
+
+}
